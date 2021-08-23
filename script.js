196fde5a4d839f0e070d08599a46af0ad6447cb5
tamtlis-kedy <86045555+tamtlis-kedy@users.noreply.github.com>
2021-08-23 13:26:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 document.body.style.display = "block"
 
-var divs = document.getElementsByTagName("img");
-for(var i = 0; i < divs.length; i++){
-   divs[i].src = "https://c.tenor.com/EGarfj-9xXMAAAAC/the-vibin-cheese-dance.gif"
-}
-var divs = document.getElementsByTagName("div");
-for(var i = 0; i < divs.length; i++){
-   divs[i].style.background = "url('https://c.tenor.com/EGarfj-9xXMAAAAC/the-vibin-cheese-dance.gif')"
-}
-
-
 setInterval(() => {
     document.getElementById("bas").style.height = window.innerHeight + "px"
     document.getElementById("bas").style.width = "100%"
